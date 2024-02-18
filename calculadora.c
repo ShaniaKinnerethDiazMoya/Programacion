43c6d54e8c46aa60dc1139d79871d2f82a27d7a3
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-02-17 22:26:37 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main(){
+  int a=8;
+  int b=4;
+  res=0;
+  resm= a*b;
+  ress= a+b;
+  printf("\n");
+  printf("El resultado de la multiplicacion de $d * %D es: %d", a , b , resm; 
+  printf("\n");
+  printf("El resultado de la suma de %d + %d es: %d", a , b , ress;
+  printf("\n");
+  
+  return 0;
+}
