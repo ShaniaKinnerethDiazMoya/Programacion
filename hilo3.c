18147720fc9a62dfb9a35c1afbc1d654e571042c
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-05-03 23:22:55 -0600

    Create hilo3.c

---
+++ hilo3.c
@@ -0,0 +1,35 @@
+public class MultiThreadExample implements Runnable {
+    private String threadName;
+
+    public MultiThreadExample(String threadName) {
+        this.threadName = threadName;
+    }
+
+    public void run() {
+        System.out.println("Hilo " + threadName + " está corriendo.");
+        try {
+            Thread.sleep(500);  // Simula una tarea que toma 500 milisegundos
+        } catch (InterruptedException e) {
+            System.out.println("Hilo " + threadName + " interrumpido.");
+        }
+        System.out.println("Hilo " + threadName + " terminó.");
+    }
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[5];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new MultiThreadExample("Thread " + (i + 1)));
+            threads[i].start();
+        }
+
+        for (Thread t : threads) {
+            try {
+                t.join();  // Espera a que el hilo t termine
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Todos los hilos han terminado. Continuando con el hilo principal.");
+    }
+}
